5378cb64e539f708d198bd48145dd402e418044b
Jolan Plaum <jolan.plaum@student.howest.be>
2022-11-17 15:58:29 +0100

    Updated Renderer

--- source/Renderer.cpp
+++ source/Renderer.cpp
@@ -77,63 +77,7 @@ void Renderer::Render()
 		Vertex v1{ vertices_screen[3 * index + 1] };
 		Vertex v2{ vertices_screen[3 * index + 2] };
 
-		Vector2 edge0 = v2.position.GetXY() - v1.position.GetXY();
-		Vector2 edge1 = v0.position.GetXY() - v2.position.GetXY();
-		Vector2 edge2 = v1.position.GetXY() - v0.position.GetXY();
-
-		int left{ (int)std::min(v0.position.x, std::min(v1.position.x, v2.position.x)) };
-		int top{  (int)std::min(v0.position.y, std::min(v1.position.y, v2.position.y)) };
-		int right{  (int)ceilf(std::max(v0.position.x, std::max(v1.position.x, v2.position.x))) };
-		int bottom{ (int)ceilf(std::max(v0.position.y, std::max(v1.position.y, v2.position.y))) };
-
-		if (left < 0) left = 0;
-		if (top < 0) top = 0;
-		if (right >= m_Width) right = m_Width - 1;
-		if (bottom >= m_Height) bottom = m_Height - 1;
-
-		for (int px{left}; px < right; ++px)
-		{
-			for (int py{top}; py < bottom; ++py)
-			{
-				float w0, w1, w2;
-				Vector2 pixel{ (float)px, (float)py };
-
-				//Check if pixel is inside triangle
-				Vector2 pixelToSide = pixel - v0.position.GetXY();
-				if ((w2 = Vector2::Cross(edge2, pixelToSide)) < 0.f) continue;
-
-				pixelToSide = pixel - v1.position.GetXY();
-				if ((w0 = Vector2::Cross(edge0, pixelToSide)) < 0.f) continue;
-
-				pixelToSide = pixel - v2.position.GetXY();
-				if ((w1 = Vector2::Cross(edge1, pixelToSide)) < 0.f) continue;
-
-				//Turn weights into ratios
-				float total = w0 + w1 + w2;
-				w0 /= total;
-				w1 /= total;
-				w2 /= total;
-
-				//Calculate depth
-				float depth = v0.position.z * w0 + v1.position.z * w1 + v2.position.z * w2;
-
-				//Depth Test
-				if (depth < m_pDepthBufferPixels[px + (py * m_Width)])
-				{
-					//Depth Write
-					m_pDepthBufferPixels[px + (py * m_Width)] = depth;
-
-					//Update Color in Buffer
-					ColorRGB finalColor{ v0.color * w0 + v1.color * w1 + v2.color * w2 };
-					finalColor.MaxToOne();
-
-					m_pBackBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBackBuffer->format,
-						static_cast<uint8_t>(finalColor.r * 255),
-						static_cast<uint8_t>(finalColor.g * 255),
-						static_cast<uint8_t>(finalColor.b * 255));
-				}
-			}
-		}
+		RenderTriangle(v0, v1, v2);
 	}
 
 	//@END
@@ -206,7 +150,6 @@ void Renderer::RenderTriangle(const Vertex& v0, const Vertex& v1, const Vertex& 
 
 void Renderer::VertexTransformationFunction(const std::vector<Vertex>& vertices_in, std::vector<Vertex>& vertices_out) const
 {
-	//Todo > W1 Projection Stage
 	vertices_out.reserve(vertices_in.size());
 
 	for (int i{}; i < vertices_in.size(); ++i)
